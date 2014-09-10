29ffdde1b44f5606a96f3471fa48646c014417e1
Jeff L <jeff@rubbingalcoholic.com>
2014-09-09 22:21:48 -0700

    undo extra call last param

--- political_data.py
+++ political_data.py
@@ -158,12 +158,12 @@ class PoliticalData():
             first_call = format_special_call(first_call_name, first_call_number)
             member_ids.insert(0, first_call)
 
-        if campaign.get('extra_last_call_name') and \
-                campaign.get('extra_last_call_num'):
-            last_call = format_special_call(
-                campaign.get('extra_last_call_name'),
-                str(campaign.get('extra_last_call_num')))
-            member_ids.extend([last_call])
+        # if campaign.get('extra_last_call_name') and \
+        #         campaign.get('extra_last_call_num'):
+        #     last_call = format_special_call(
+        #         campaign.get('extra_last_call_name'),
+        #         str(campaign.get('extra_last_call_num')))
+        #     member_ids.extend([last_call])
 
         print member_ids
 
