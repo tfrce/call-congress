ad4a60b99b2de5d9cbc53d3dcaf514b4596c87a4
Josh Levinger <josh@levinger.net>
2015-04-28 16:53:09 -0700

    admin templates

--- call_server/admin/views.py
+++ call_server/admin/views.py
@@ -1,3 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, jsonify
 
 admin = Blueprint('admin', __name__)
+
+
+@admin.route('/admin')
+def admin_home():
+    return render_template('admin.html')
+
+
+@admin.route('/admin/legislators.json')
+def admin_legislators():
+    return jsonify(data.legislators)
