8eefd527084e15e3eb5b0871c742677d81ac11ac
Josh Levinger <josh@levinger.net>
2015-05-06 16:45:08 -0700

    admin required decorator

---
+++ call_server/user/decorators.py
@@ -0,0 +1,15 @@
+from functools import wraps
+
+from flask import abort
+from flask.ext.login import current_user
+
+from contants import ADMIN
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.role_code != ADMIN:
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
