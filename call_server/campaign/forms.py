3276540a7f7eaa8ea832d5c3884fb088d0082b0b
Josh Levinger <josh@levinger.net>
2015-05-08 02:32:29 -0700

    preload us states

--- call_server/campaign/forms.py
+++ call_server/campaign/forms.py
@@ -10,6 +10,7 @@ from wtforms.validators import Required, AnyOf
 from .constants import (CAMPAIGN_CHOICES, CAMPAIGN_NESTED_CHOICES,
                         TARGET_BY_CHOICES, ORDERING_CHOICES,
                         EMPTY_CHOICES)
+from ..political_data.constants import US_STATES
 
 from .models import PhoneNumber
 
@@ -20,12 +21,13 @@ class CampaignForm(Form):
     next = HiddenField()
     name = TextField(_('Campaign Name'), [Required()])
     campaign_type = SelectField(_('Campaign Type'), choices=choice_items(CAMPAIGN_CHOICES))
+    campaign_state = SelectField(_('State'), choices=choice_items(US_STATES))
     campaign_subtype = SelectField('', [AnyOf(choice_values_flat(CAMPAIGN_NESTED_CHOICES))],
                                        choices=choice_items(EMPTY_CHOICES))
     # nested_type passed to data-field in template, but starts empty
 
     target_by = RadioField(_('Target By'), choices=choice_items(TARGET_BY_CHOICES))
-    target_set = SelectMultipleField(_('Set Targets'), choices=choice_items(EMPTY_CHOICES))
+    target_set = SelectMultipleField(_('or Set Targets'), choices=choice_items(EMPTY_CHOICES))
     target_ordering = RadioField(_('Order'), choices=choice_items(ORDERING_CHOICES))
 
     call_maximum = IntegerField(_('Call Maximum'))
