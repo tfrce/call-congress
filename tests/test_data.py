1a001db46513920a458508d4a438cb764bccf01c
Josh Levinger <josh@levinger.net>
2015-07-13 09:48:22 -0700

    test update

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,33 +9,34 @@ class TestData(BaseTestCase):
 
         self.cache = legislators.copy()
         self.cache.update(districts)
+        # mock the flask-cache as a dictionary
 
     def test_cache(self):
         self.assertIsNotNone(self.cache)
 
     def test_districts(self):
-        district = self.cache['us:zipcode:94612'][0]
+        district = self.cache.get('us:zipcode:94612')[0]
         self.assertEqual(district['state'], 'CA')
         self.assertEqual(district['house_district'], '13')
 
     def test_district_multiple_states(self):
-        district = self.cache['us:zipcode:53811']
+        district = self.cache.get('us:zipcode:53811')
         self.assertEqual(len(district), 4)
 
     def test_senate(self):
-        senator = self.cache['us:senate:CA'][0]
+        senator = self.cache.get('us:senate:CA')[0]
         self.assertEqual(senator['chamber'], 'senate')
         self.assertEqual(senator['state'], 'CA')
         self.assertEqual(senator['in_office'], '1')
 
     def test_house(self):
-        rep = self.cache['us:house:CA:13'][0]
+        rep = self.cache.get('us:house:CA:13')[0]
         self.assertEqual(rep['chamber'], 'house')
         self.assertEqual(rep['state'], 'CA')
         self.assertEqual(rep['district'], '13')
         self.assertEqual(rep['in_office'], '1')
 
-    def test_locate_member_ids(self):
+    def test_locate_targets(self):
         ids = lookup.locate_targets('98004', cache=self.cache)
 
         self.assertEqual(len(ids), 4)
