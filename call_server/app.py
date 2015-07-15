5d4c6327f1da333758924a30b327a582fd69e5ea
Josh Levinger <josh@levinger.net>
2015-07-14 18:39:26 -0700

    update sqlalchemy for new naming convention per https://groups.google.com/forum/#!topic/sqlalchemy/SIT4D8S9dUg

--- call_server/app.py
+++ call_server/app.py
@@ -85,14 +85,13 @@ def configure_app(app, configuration=None):
         app.config.from_object(env_config)
 
 
-
 def init_extensions(app):
     db.init_app(app)
     db.app = app
     db.metadata.naming_convention = {
       "ix": 'ix_%(column_0_label)s',
       "uq": "uq_%(table_name)s_%(column_0_name)s",
-      "ck": "ck_%(table_name)s_%(constraint_name)s",
+      "ck": "ck_%(table_name)s_%(column_0_name)s",
       "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
       "pk": "pk_%(table_name)s"
     }
