a1e1304148a9bc49a52d87673229dff0fd43e6ba
Josh Levinger <josh@levinger.net>
2015-08-10 13:09:17 -0700

    non-crud api route for campaign stats.json

--- call_server/api/views.py
+++ call_server/api/views.py
@@ -1,21 +1,16 @@
-from flask import current_app, abort, request
-from flask.ext.login import current_user
-from flask.ext.restless import ProcessingException
+import json
+from flask import Blueprint, Response
+from sqlalchemy.sql import func
 
-from ..extensions import rest
+from decorators import api_key_or_auth_required, restless_api_auth
+
+from ..extensions import rest, db
 from ..campaign.models import Campaign, Target, AudioRecording
+from ..call.models import Call
 
 
-def restless_api_auth(*args, **kwargs):
-    if current_user.is_authenticated():
-        return True  # allow
+api = Blueprint('api', __name__, url_prefix='/api')
 
-    # check for system api key
-    admin_key = current_app.config.get('ADMIN_API_KEY')
-    if admin_key and request.values.get('api_key') == admin_key:
-        return True
-
-    abort(401, 'Not Authenticated')
 
 restless_preprocessors = {'GET_SINGLE':   [restless_api_auth],
                           'GET_MANY':     [restless_api_auth],
@@ -37,3 +32,19 @@ def configure_restless(app):
                     include_columns=['id', 'key', 'version', 'description',
                                      'text_to_speech', 'selected', 'hidden'],
                     include_methods=['file_url', 'selected_campaigns', 'selected_campaign_ids'])
+
+# non CRUD-routes
+# protect with decorator
+@api.route('/campaign/<int:campaign_id>/stats.json', methods=['GET'])
+@api_key_or_auth_required
+def campaign_stats(campaign_id):
+    campaign = Campaign.query.filter_by(id=campaign_id).first_or_404()
+    calls_by_day_query = (db.session.query(func.date(Call.timestamp), func.count(Call.id))
+            .filter(Call.campaign == campaign)
+            .group_by(func.date(Call.timestamp)))
+    calls_by_day = dict(name='Calls by Day', data=dict(calls_by_day_query.all()))
+
+    total_calls_query = Call.query.filter(Call.campaign == campaign)
+    total_calls = dict(name='Total Calls', data=total_calls_query.count())
+
+    return Response(json.dumps([total_calls, calls_by_day]), mimetype='application/json')
