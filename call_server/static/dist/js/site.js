c374a56fd3b78b701efa10a2f31182910ec2db5a
Josh Levinger <josh@levinger.net>
2015-09-27 14:37:57 -0700

    fix sunlight search for custom congress campaigns

--- call_server/static/dist/js/site.js
+++ call_server/static/dist/js/site.js
@@ -1202,6 +1202,17 @@ $(document).ready(function () {
       if (campaign_type === 'congress') {
         // hit Sunlight OpenCongress v3
 
+        //convert generic chamber names to House / Senate
+        if (chamber === 'lower') {
+          chamber = 'house';
+        }
+        if (chamber === 'upper') {
+          chamber = 'senate';
+        }
+        if (chamber === 'both') {
+          chamber = '';
+        }
+
         $.ajax({
           url: CallPower.Config.SUNLIGHT_CONGRESS_URL,
           data: {
